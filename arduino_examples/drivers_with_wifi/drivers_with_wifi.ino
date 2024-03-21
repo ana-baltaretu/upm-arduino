afc288bb682819708d75bbe61910474c72a01f5d
Ana Băltărețu <a.baltaretu@student.tudelft.nl>
2024-03-21 16:23:43 +0100

    Add working code arduino

---
+++ arduino_examples/drivers_with_wifi/drivers_with_wifi.ino
@@ -0,0 +1,74 @@
+#include <Stepper.h>
+#include <WiFiNINA.h>
+
+char ssid[] = "Coldspotttt";
+char pass[] = "password1234";
+WiFiServer server(80); // Port 80
+
+int stepsPerRevolution = 100;  // change this to fit the number of steps per revolution
+char val;
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
+Stepper myStepper1(stepsPerRevolution, 5, 3, 4, 2);
+Stepper myStepper2(stepsPerRevolution, 9, 7, 8, 6);
+
+void setup() {
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  // set the speed at 60 rpm:
+  myStepper1.setSpeed(60);
+  myStepper2.setSpeed(60);
+  // initialize the serial port:
+  Serial.begin(9600);
+  server.begin();
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  
+  if (client) {
+    // Serial.println("New client connected");
+    client.println("Hello from Arduino!");
+
+    if (stepsPerRevolution == 0 && client.available()) {
+      val = client.read();
+      // Serial.println(val);
+      if (val == 'F') {
+        // Serial.println("Setting to high");
+        digitalWrite(13, HIGH);
+        stepsPerRevolution = -100;
+      } else if (val == 'B') {
+        digitalWrite(13, HIGH);
+        stepsPerRevolution = 100;
+      } else {
+        // Serial.println("Setting to low");
+        digitalWrite(13, LOW);
+        stepsPerRevolution = 0;
+      }
+    }
+
+    // client.stop();
+    // Serial.println("Client disconnected");
+  }
+
+
+  if (stepsPerRevolution > 0){ // drive backwards
+    //Serial.println(stepsPerRevolution);
+    myStepper1.step(1);
+    myStepper2.step(1);
+    stepsPerRevolution--;
+  } 
+  if (stepsPerRevolution < 0){ // drive forward
+    //Serial.println(stepsPerRevolution);
+    myStepper1.step(-1);
+    myStepper2.step(-1);
+    stepsPerRevolution++;
+  }
+
+}
